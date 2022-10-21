b0d0257f036a56ae49fa6a4e6d164112418f87a8
Nikhilkb2255 <115878497+Nikhilkb2255@users.noreply.github.com>
2022-10-21 10:21:10 +0530

    Add files via upload

---
+++ Python lab cycle 1/degree.py
@@ -0,0 +1,4 @@
+d=float(input("Enter degree celsius :"))
+f=d*2+30
+print("Celsius : ",d)
+print("Fahrenheit : ",f)
---
+++ Python lab cycle 1/swaping.py
@@ -0,0 +1,8 @@
+a=int(input("Enter 1st number : "))
+b=int(input("Enter 2st number : "))
+print(a,b)
+a=a+b
+b=a-b
+a=a-b
+print("After swapping")
+print(a,b)
